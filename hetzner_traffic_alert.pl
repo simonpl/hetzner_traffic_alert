7eb831147e68326c9d63a07be8c2bfe4dca3a938
simonp <simon.plasger@web.de>
2013-07-21 17:48:51 +0200

    Added comments

--- hetzner_traffic_alert.pl
+++ hetzner_traffic_alert.pl
@@ -32,9 +32,9 @@ $browser->credentials(
     $traffic_alert_configuration::realm,
     $traffic_alert_configuration::user,
     $traffic_alert_configuration::password
-);
+); # Initialize user agent with options defined in configuration
 my $request = new HTTP::Request('POST',$traffic_alert_configuration::url);
-$request->content_type("application/x-www-form-urlencoded");
+$request->content_type("application/x-www-form-urlencoded"); # Initialize Request
 my $content = 'type=month&from='.strftime("%Y-%m", localtime).'-01&to='.strftime("%Y-%m", localtime).'-31';
 foreach(@traffic_alert_configuration::ips)
 {
@@ -44,23 +44,23 @@ foreach(@traffic_alert_configuration::subnets)
 {
     $content .= '&subnet[]='.$_;
 }
-$request->content($content);
-my $response = $browser->request($request);
-my $obj = JSON->new->utf8->decode($response->content);
+$request->content($content); # Put IPs and subnets into the POST request
+my $response = $browser->request($request); # Execute the POST request
+my $obj = JSON->new->utf8->decode($response->content); # Get the JSON Data as an object
 my $sumtraffic = 0;
 my $key;
-while($key = each $obj->{"traffic"}->{"data"})
+while($key = each $obj->{"traffic"}->{"data"}) # Count the outgoing traffic
 {
     $sumtraffic += $obj->{"traffic"}->{"data"}->{$key}->{"out"};
 }
 print "Outgoing traffic this month so far: ".$sumtraffic." GB\n";
-my $left = $traffic_alert_configuration::allowed - $sumtraffic;
+my $left = $traffic_alert_configuration::allowed - $sumtraffic; # Calculate how much traffic is left for this month
 print "You have ".$left." GB of outgoing traffic remaining this month.";
 print "\n";
-if($left < $traffic_alert_configuration::warn_threshold * $traffic_alert_configuration::allowed * 0.01)
+if($left < $traffic_alert_configuration::warn_threshold * $traffic_alert_configuration::allowed * 0.01) # Is the threshold reached?
 {
     print "Warning: You have less than ".$traffic_alert_configuration::warn_threshold."% of your monthly allowd traffic amount left!\n";
-    if($traffic_alert_configuration::trigger)
+    if($traffic_alert_configuration::trigger) # Should a command be triggered?
     {
         system($traffic_alert_configuration::triggercom);
     }
